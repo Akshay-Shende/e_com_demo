ff80d7606d51fa0da5a5ca30a78ebbe6d0be9748
hp <hp@DESKTOP-SVA3L1T>
2024-08-07 10:53:22 +0530

    changes for header and footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -2,9 +2,34 @@ import React from 'react'
 
 const Footer = () => {
   return (
+   <footer className='container'>
+    <div className='grid grid-cols-3 gap-4 text-center'>
     <div>
-      This is footer component
+                <h2 class="text-lg font-semibold mb-4">Company</h2>
+                <ul className='list-none pl-0'>
+                    <li className=''><a href="#" class="text-gray-400 hover:text-white">About Us</a></li>
+                    <li><a href="#" class="text-gray-400 hover:text-white">Careers</a></li>
+                    <li><a href="#" class="text-gray-400 hover:text-white">Press</a></li>
+                </ul>
+            </div>
+            <div>
+                <h2 class="text-lg font-semibold mb-4">Resources</h2>
+                <ul>
+                    <li><a href="#" class="text-gray-400 hover:text-white">Blog</a></li>
+                    <li><a href="#" class="text-gray-400 hover:text-white">Help Center</a></li>
+                    <li><a href="#" class="text-gray-400 hover:text-white">Guides</a></li>
+                </ul>
+                </div>
+                <div>
+                <h2 class="text-lg font-semibold mb-4">Contact</h2>
+                <ul>
+                    <li><a href="#" class="text-gray-400 hover:text-white">Email</a></li>
+                    <li><a href="#" class="text-gray-400 hover:text-white">Twitter</a></li>
+                    <li><a href="#" class="text-gray-400 hover:text-white">LinkedIn</a></li>
+                </ul>
+            </div>       
     </div>
+   </footer>
   )
 }
 
--- src/app/components/header.js
+++ src/app/components/header.js
@@ -2,10 +2,15 @@ import React from "react";
 import Link from "next/link";
 const Header = () => {
   return (
-   <nav>
-    <div className="brand"></div>
-    <div className="">
-        This is Header 
+   <nav className=" bg-slate-200 py-4 ">
+    <div className="container- mx-auto flex items-center justify-between px-4">
+    <div className="brand">E-Com</div>
+       <ul className="flex ml-auto">
+        <li className="px-4"><a href="" className="text-black hover:text-slate-400">Home</a></li>
+        <li className="px-4"><a href="" className="text-black hover:text-slate-400">About Us</a></li>
+        <li className="px-4"><a href="" className="text-black hover:text-slate-400">Contact Us</a></li>
+        <li className="px-4"><a href="" className="text-black hover:text-slate-400">Log In</a></li>
+      </ul> 
     </div>
    </nav>
   );
